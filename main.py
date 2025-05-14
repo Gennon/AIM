24bc101220317c3c7fe998e51b42b9d620ecba3a
Kim Teigen <kim.teigen@sparebank1.no>
2025-05-14 08:13:52 +0200

    feat: Add support for inserting items

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ llm = ChatOllama(model="mistral", temperature=0.5)
 # Initialize the database connection
 db = SQLDatabase.from_uri("sqlite:///Chinook.db", sample_rows_in_table_info=0)
 
-@app.route('/get', methods=['GET'])
+@app.route('/query', methods=['GET'])
 def get():
     user_request = json.dumps(request.json)
 
@@ -49,6 +49,46 @@ def get():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
     
+
+@app.route('/query', methods=['POST'])
+def insert():
+    user_request = json.dumps(request.json)
+
+    messages = [
+        (
+            "system",
+            f"""
+            Your task is to generate SQL queries based on user requests. 
+            You are a helpful assistant that can understand natural language and convert it into SQL queries. 
+            You should only create queries based on the tables and columns in the datbase {db.get_table_info()}.
+            You should always use explicit column names in your queries, like TableName.ColumnName.
+            You should never drop tables.
+            You should return a maximum of 1 SQL query.
+            You should only respond with the SQL query and nothing else.
+            You should only modify data in the database by adding items and not retrieve it.
+            If you can't generate a SQL query, respond with "NULL".
+            Thank you!
+            """,
+        ),
+        ("human", user_request),
+    ]
+    sql_message = llm.invoke(messages)
+
+    if not sql_message.content or sql_message.content.strip().upper() == "NULL":
+        return jsonify({'error': 'Unable to generate SQL query'}), 400
+    # Execute the SQL query
+    print(sql_message.content)
+    try:
+        result = db.run(sql_message.content, include_columns=True)   
+        
+        # Convert the result to JSON
+        if result is "":
+          return jsonify({'query': sql_message.content,'result': 'success'}), 201
+        else:
+          data = ast.literal_eval(result)
+          return jsonify({'query': sql_message.content,'result': data}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
     
 
