73ef382f4d898c0d0f1b3a765a3bb0f3313e5f47
Kim Teigen <kim.teigen@sparebank1.no>
2025-05-14 15:57:22 +0200

    refactor: Simplify system prompts

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from langchain_ollama import ChatOllama
-from langchain_core.messages import AIMessage
 from langchain_community.utilities import SQLDatabase
 import json
 import ast
@@ -77,69 +76,57 @@ def execute_sql_query(sql_query):
 
 @app.route('/query', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def query():
-    
-
-    # Define system prompts for each method
-    system_prompts = {
-        'GET': f"""
-            Your task is to generate SQL queries based on user requests. 
-            You are a helpful assistant that can understand natural language and convert it into SQL queries. 
-            You should only create queries based on the tables and columns in the datbase {table_info}.
-            You should always use explicit column names in your queries, like TableName.ColumnName.
-            You should never drop tables.
-            You should return a maximum of 1 SQL query.
-            You should only respond with the SQL query and nothing else.
-            You should only retrieve data from the database and not modify it.
-            If you can't generate a SQL query, respond with \"NULL\".
-            Thank you!
-        """,
-        'POST': f"""
-            Your task is to generate SQL queries based on user requests. 
-            You are a helpful assistant that can understand natural language and convert it into SQL queries. 
-            You should only create queries based on the tables and columns in the datbase {table_info}.
-            You should always use explicit column names in your queries, like TableName.ColumnName.
-            You should never drop tables.
-            You should return a maximum of 1 SQL query.
-            You should only respond with the SQL query and nothing else.
-            You should only modify data in the database by adding items.
-            You should create a query that first inserts and then selects the data.
-            If you can't generate a SQL query, respond with \"NULL\".
-            Thank you!
-        """,
-        'DELETE': f"""
-            Your task is to generate SQL queries based on user requests. 
-            You are a helpful assistant that can understand natural language and convert it into SQL queries. 
-            You should only create queries based on the tables and columns in the datbase {table_info}.
-            Do not make up any tables or columns, verify the table names and column names exist in the database.
-            You should always use explicit column names in your queries, like TableName.ColumnName.
-            You should never drop tables.
-            You should return a maximum of 1 SQL query.
-            You should only respond with the SQL query and nothing else.
-            The SQL should only delete one or more items in the database.
-            If you can't generate a SQL query, respond with \"NULL\".
-            Thank you!
-        """,
-        'PUT': f"""
-            Your task is to generate SQL queries based on user requests. 
-            You are a helpful assistant that can understand natural language and convert it into SQL queries. 
-            You should only create queries based on the tables and columns in the datbase {table_info}.
-            Do not make up any tables or columns, verify the table names and column names exist in the database.
-            You should always use explicit column names in your queries, like TableName.ColumnName.
-            You should never drop tables.
-            You should return a maximum of 1 SQL query.
-            You should only respond with the SQL query and nothing else.
-            The SQL should only update one or more items in the database.
-            You should create a query that first updates and then selects the data.
-            If you can't generate a SQL query, respond with \"NULL\".
-            Thank you!
-        """,
-    }
-
-    system_prompt = system_prompts[request.method]
+    system_prompt = get_system_prompt(request.method)
     response, status_code = handle_request(request, system_prompt)
     return jsonify(response), status_code
     
+def get_system_prompt(method):
+    
+    pre = f"""
+        Your task is to generate SQL queries based on user requests. 
+        You are a helpful assistant that can understand natural language and convert it into SQL queries. 
+        You should only create queries based on the tables and columns in the datbase {table_info}.
+        You should always use explicit column names in your queries, like TableName.ColumnName.
+        You should never drop tables.
+        You should return a maximum of 1 SQL query.
+        You should only respond with the SQL query and nothing else.
+      """
+    
+    example = f"""
+        Example:
+        SomeValue: "string" should return a SQL query that has the key SomeValue and the value should be of type "string".
+        SomeValue: "int" should return a SQL query that has the key SomeValue and the value should be of type "int".
+        "string" should never be used in the WHERE clause.
+        "int" should never be used in the WHERE clause.
+      """
+    
+    post = """
+        If you can't generate a SQL query, respond with \"NULL\".
+        Thank you!
+      """
 
+    prompts = {
+      'GET': """
+        You should only retrieve data from the database and not modify it.
+      """,
+      'POST': """
+        You should only modify data in the database by adding items.
+        You should create a query that first inserts and then selects the data.
+      """,
+      'DELETE': """
+        The SQL should only delete one or more items in the database.
+      """,
+      'PUT': """
+        The SQL should only update one or more items in the database.
+        You should create a query that first updates and then selects the data.
+      """,
+    }
+    return f"""
+      {pre}
+      {example}
+      {prompts[method]}
+      {post}
+      """
 
 if __name__ == '__main__':
     app.run(debug=True)
